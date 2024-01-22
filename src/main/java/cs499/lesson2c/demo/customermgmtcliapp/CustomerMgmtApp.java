893028f89999e3cc329697c76ad9c66d8f1cfa89
Prof OKalu <okalu1@outlook.com>
2024-01-22 15:24:59 -0600

    New project

---
+++ src/main/java/cs499/lesson2c/demo/customermgmtcliapp/CustomerMgmtApp.java
@@ -0,0 +1,8 @@
+package cs499.lesson2c.demo.customermgmtcliapp;
+
+public class CustomerMgmtApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CI/CD");
+    }
+}
---
+++ src/main/java/cs499/lesson2c/demo/customermgmtcliapp/model/Customer.java
@@ -0,0 +1,6 @@
+package cs499.lesson2c.demo.customermgmtcliapp.model;
+
+public class Customer {
+    private String customerId;
+    private String name;
+}
